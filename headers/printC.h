d68ca10b870c51bcdb764fbc2e47c4c213fa18aa
gman <omar5mahmoud3ahmed6@gmail.com>
2025-03-25 13:21:41 +0200

    add printC.h header

---
+++ headers/printC.h
@@ -0,0 +1,18 @@
+
+void printGreen(char *msg);
+
+void printRed(char *msg);
+
+void printYellow(char *msg);
+
+void printBlue(char *msg) ;
+
+void printMagenta(char *msg) ;
+
+void printCyan(char *msg);
+
+void printBold(char *msg);
+
+
+void printBoldColor(char *msg) ;
+void printGray(char *msg) ;
